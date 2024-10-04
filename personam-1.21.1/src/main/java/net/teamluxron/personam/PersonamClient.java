731fb67cdc803979f8cb8785088a367e60bd6f01
DamienBea <damienbeacume@gmail.com>
2024-10-04 03:20:16 +0200

    PersonaM 1.21.1 Fabric

---
+++ personam-1.21.1/src/main/java/net/teamluxron/personam/PersonamClient.java
@@ -0,0 +1,10 @@
+package net.teamluxron.personam;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PersonamClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
